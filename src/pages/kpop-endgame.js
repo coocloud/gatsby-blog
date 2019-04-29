2a824e5f6a93ab040b8f5154d21b4bafeedb0177
coocloud <apple19903@gmail.com>
2019-04-29 15:46:12 +0200

    add page for kpop endgame; add global and component styles

---
+++ src/pages/kpop-endgame.js
@@ -0,0 +1,165 @@
+import React from 'react'
+import Tachyons from "../components/tachyons"
+
+const KpopEndgame = () => (
+    <div className="kpop">
+        <section className="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+            <h1 className="mt0">KPOP Endgame</h1>
+            <p className="lh-copy measure clickable">
+                <img className="google" src="https://www.google.com/logos/fnbx/thanos/thanos_idle.png" alt="Google's Thanos infinity gauntlet"/>
+                <p>Google's Thanos's Infinity Gauntlet</p>
+            </p>
+        </section>
+
+        <Tachyons>
+            <article>
+                <h2 className="f3 fw4 pa3 mv0">Artists</h2>
+                <div className="cf pa2">
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/yoona__lim/"
+                           className="db link dim tc">
+                            <img
+                                src="https://wiki.d-addicts.com/images/9/9a/YoonA.jpg"
+                                alt="Yoona Im" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Yoona Im</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/jessica.syj/?hl=en"
+                           className="db link dim tc">
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Jessica_on_the_CLEO_Thailand_magazine_%28cropped%29.png/250px-Jessica_on_the_CLEO_Thailand_magazine_%28cropped%29.png"
+                                alt="Jessica Jung" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100 ttu">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Jessica Jung</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/taeyeon_ss/"
+                           className="db link dim tc">
+                            <img
+                                src="https://channel-korea.com/wp-content/uploads/2018/07/taeyeon.jpg"
+                                alt="Taeyeon Kim"
+                                className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Taeyeon Kim</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/tiffanyyoungofficial/"
+                           className="db link dim tc">
+                            <img
+                                src="http://images5.fanpop.com/image/photos/26100000/Tiffany-in-Run-Devil-Run-MV-tiffany-girls-generation-26194320-500-282.jpg"
+                                alt="Tiffany Young" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Tiffany Young</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/515sunnyday/"
+                           className="db link dim tc">
+                            <img
+                                src="https://www.allkpop.com/upload/2017/12/af_org/28055730/Girls-Generation-Sunny.jpg"
+                                alt="Sunny" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Sunny</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/seojuhyun_s/"
+                           className="db link dim tc">
+                            <img
+                                src="https://channel-korea.com/wp-content/uploads/2018/04/Seohyun-SNSD.jpg"
+                                alt="Seohyun channel korea" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Seohyun</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/yulyulk/"
+                           className="db link dim tc">
+                            <img
+                                src="http://asianwiki.com/images/4/49/Kwon_Yuri-p01.jpg"
+                                alt="Yuri AsianWiki" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Yuri Kwon</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/sooyoungchoi/"
+                           className="db link dim tc">
+                            <img
+                                src="https://www.allkpop.com/upload/2018/01/af_org/03055852/Girls-Generation-Sooyoung.jpg"
+                                alt="Sooyoung Allkpop" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Sooyoung Choi</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/watasiwahyo/"
+                           className="db link dim tc">
+                            <img
+                                src="https://www.allkpop.com/upload/2018/03/af_org/29102610/Girls-Generation-Hyoyeon.jpg"
+                                alt="Hyoyeon Allkpop" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">SNSD</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Hyoyeon</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-50 w-25-m w-20-l pa2">
+                        <a href="https://www.instagram.com/sooyaaa__/"
+                           className="db link dim tc">
+                            <img
+                                src="https://koreaboo-cdn.storage.googleapis.com/2017/07/jisoo1.jpg"
+                                alt="Jisoo Koreaboo" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 black truncate w-100">Blackpink</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Jisoo</dd>
+                            </dl>
+                        </a>
+                    </div>
+                </div>
+            </article>
+        </Tachyons>
+    </div>
+)
+
+export default KpopEndgame
