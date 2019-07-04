4692d2aa6e9e7e0afc2f28ba8b956f173ad4bcc9
coocloud <apple19903@gmail.com>
2019-07-04 16:23:18 +0200

    remove comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -119,24 +119,6 @@ class BlogIndex extends React.Component {
                     )
                 })}
 
-                {/*{posts.map(({node}) => {*/}
-                {/*    const title = get(node, 'frontmatter.title') || node.fields.slug*/}
-                {/*    return (*/}
-                {/*        <div key={node.fields.slug}>*/}
-                {/*            <h3*/}
-                {/*                style={{*/}
-                {/*                    marginBottom: rhythm(1 / 4),*/}
-                {/*                }}*/}
-                {/*            >*/}
-                {/*                <Link style={{boxShadow: 'none'}} to={node.fields.slug}>*/}
-                {/*                    {title}*/}
-                {/*                </Link>*/}
-                {/*            </h3>*/}
-                {/*            <small>{node.frontmatter.date}</small>*/}
-                {/*            <p dangerouslySetInnerHTML={{__html: node.excerpt}}/>*/}
-                {/*        </div>*/}
-                {/*    )*/}
-                {/*})}*/}
             </Layout>
         )
     }
