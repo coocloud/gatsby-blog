70330cf4d871303a8c9830851f819284dd5b89e3
coocloud <apple19903@gmail.com>
2019-07-04 14:58:16 +0200

    remove default pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `coocloud Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
