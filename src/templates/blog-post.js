32bf871cea351ab8254f35b5d7336ed9a337d0ab
coocloud <apple19903@gmail.com>
2019-07-04 18:30:29 +0200

    Revert "modify blog post template"
    This reverts commit 404bffaa

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,71 +9,65 @@ import { rhythm, scale } from '../utils/typography'
 
 class BlogPostTemplate extends React.Component {
   render() {
-    // const post = this.props.data.markdownRemark
+    const post = this.props.data.markdownRemark
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
     const siteDescription = post.excerpt
     const { previous, next } = this.props.pageContext
 
-      return (
-          <div>
-              No Content
-          </div>
-      )
+    return (
+      <Layout location={this.props.location}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'description', content: siteDescription }]}
+          title={`${post.frontmatter.title} | ${siteTitle}`}
+        />
+        <h1>{post.frontmatter.title}</h1>
+        <p
+          style={{
+            ...scale(-1 / 5),
+            display: 'block',
+            marginBottom: rhythm(1),
+            marginTop: rhythm(-1),
+          }}
+        >
+          {post.frontmatter.date}
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <hr
+          style={{
+            marginBottom: rhythm(1),
+          }}
+        />
+        <Bio />
 
-    // return (
-    //   <Layout location={this.props.location}>
-    //     <Helmet
-    //       htmlAttributes={{ lang: 'en' }}
-    //       meta={[{ name: 'description', content: siteDescription }]}
-    //       title={`${post.frontmatter.title} | ${siteTitle}`}
-    //     />
-    //     <h1>{post.frontmatter.title}</h1>
-    //     <p
-    //       style={{
-    //         ...scale(-1 / 5),
-    //         display: 'block',
-    //         marginBottom: rhythm(1),
-    //         marginTop: rhythm(-1),
-    //       }}
-    //     >
-    //       {post.frontmatter.date}
-    //     </p>
-    //     <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    //     <hr
-    //       style={{
-    //         marginBottom: rhythm(1),
-    //       }}
-    //     />
-    //     <Bio />
-    //
-    //     <ul
-    //       style={{
-    //         display: 'flex',
-    //         flexWrap: 'wrap',
-    //         justifyContent: 'space-between',
-    //         listStyle: 'none',
-    //         padding: 0,
-    //       }}
-    //     >
-    //       <li>
-    //         {
-    //           previous &&
-    //           <Link to={previous.fields.slug} rel="prev">
-    //             ← {previous.frontmatter.title}
-    //           </Link>
-    //         }
-    //       </li>
-    //       <li>
-    //         {
-    //           next &&
-    //           <Link to={next.fields.slug} rel="next">
-    //             {next.frontmatter.title} →
-    //           </Link>
-    //         }
-    //       </li>
-    //     </ul>
-    //   </Layout>
-    // )
+        <ul
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            listStyle: 'none',
+            padding: 0,
+          }}
+        >
+          <li>
+            {
+              previous &&
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            }
+          </li>
+          <li>
+            {
+              next &&
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            }
+          </li>
+        </ul>
+      </Layout>
+    )
   }
 }
 
@@ -85,6 +79,15 @@ export const pageQuery = graphql`
       siteMetadata {
         title
         author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
       }
     }
   }
