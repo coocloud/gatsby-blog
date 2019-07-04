0eaa44eb0c23dd0fbd8497c90283b120f23beae2
coocloud <apple19903@gmail.com>
2019-07-04 18:30:44 +0200

    Revert "remove markdown remark query"
    This reverts commit 6f0cb6a5

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,26 +49,26 @@ exports.createPages = ({ graphql, actions }) => {
         }
 
         // Create blog posts pages.
-        // const posts = result.data.allMarkdownRemark.edges;
+        const posts = result.data.allMarkdownRemark.edges;
         const prismicPosts = result.data.allPrismicPage.edges;
         const prismicSocialPosts = result.data.allPrismicSocialpage.edges;
 
         console.log(prismicPosts);
 
-        // _.each(posts, (post, index) => {
-        //   const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-        //   const next = index === 0 ? null : posts[index - 1].node;
-        //
-        //   createPage({
-        //     path: post.node.fields.slug,
-        //     component: blogPost,
-        //     context: {
-        //       slug: post.node.fields.slug,
-        //       previous,
-        //       next,
-        //     },
-        //   })
-        // })
+        _.each(posts, (post, index) => {
+          const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+          const next = index === 0 ? null : posts[index - 1].node;
+
+          createPage({
+            path: post.node.fields.slug,
+            component: blogPost,
+            context: {
+              slug: post.node.fields.slug,
+              previous,
+              next,
+            },
+          })
+        })
 
           prismicPosts.forEach(({ node }) => {
             const slugPath = node.slugs[0];
